46bb80e1d74a8f9338721e215ca13fc09a27251b
GG <45474205+xionggor@users.noreply.github.com>
2026-04-23 22:24:52 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,22 @@
+const CACHE_NAME = 'sbmta-v4-cache';
+const urlsToCache = [
+  './',
+  './index.html',
+  './stations.js',
+  'https://cdn.jsdelivr.net/npm/protobufjs@7.2.5/dist/protobuf.min.js',
+  'https://cdn.jsdelivr.net/npm/sortablejs@latest/Sortable.min.js'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
